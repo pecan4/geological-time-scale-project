1a31023d7609afbb1abe0920928811004fb28364
J4maR+y <76506548+jamarijohnson@users.noreply.github.com>
2024-10-23 12:31:06 -0400

    made badly drawn dino

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "tiny badly drawn dinosaur":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . b b c . . . . . . . 
+. . . . . b b c b b b b . . . . 
+. . . . b b c b b f b b b b b . 
+. . . . b c b b b b b b b . . . 
+. . . . b c b b b b b . . . . . 
+. . . . b c b b b b . . . . . . 
+. . . b c b b b b b b b . . . . 
+. . . b c b b b b b . . b . . . 
+. . b c b b b b b b . . . . . . 
+c c c b b b b b b . . . . . . . 
+. . . . b b b b . . . . . . . . 
+. . . . b . . b . . . . . . . . 
+. . . . b . . b . . . . . . . . 
+. . . . b b . b b . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
