88fc5c06790789eac07cac12d81c5a1e4ced1b50
pecan4 <157906054+pecan4@users.noreply.github.com>
2024-10-23 13:27:31 -0400

    made a tiny bacteria sim

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+let mySprite = sprites.create(transformSprites.scale2x(assets.image`lil bacteria`), SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level2`)
+forever(function () {
+    if (controller.right.isPressed()) {
+        transformSprites.changeRotation(mySprite, 3)
+    } else if (controller.left.isPressed()) {
+        transformSprites.changeRotation(mySprite, -3)
+    } else if (controller.up.isPressed()) {
+    	
+    } else {
+    	
+    }
+    spriteutils.setVelocityAtAngle(mySprite, spriteutils.degreesToRadians(transformSprites.getRotation(mySprite)), 100)
+})
